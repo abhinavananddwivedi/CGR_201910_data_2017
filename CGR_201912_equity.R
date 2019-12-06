92ae7c410f8c11204935f648edcea9e675d24e07
abhinavananddwivedi <abhinavananddwivedi@gmail.com>
2019-12-06 18:37:55 +0530

    LHS and RHS nested

--- CGR_201912_equity.R
+++ CGR_201912_equity.R
@@ -11,12 +11,11 @@ library(lubridate)
 
 ### Reading the data file ###
 
-df_equity <- readr::read_csv('Data_equity_2018_clean.csv', 
+df_equity <- readr::read_csv('CGR_equity_2019.csv', 
                              na = c("", "NA", ".", " "),
                              col_names = T,
                              col_types = cols(.default = col_double(), 
-                                              Date = col_date(format = "%d/%m/%Y"))) %>%
-  dplyr::select(-starts_with('X'))
+                                              Date = col_date(format = "%d/%m/%Y")))
 
 
 ### Removing empty columns ###
@@ -36,14 +35,22 @@ func_full_NA_col_killer <- function(data_frame)
 df_equity_clean <- df_equity %>% func_full_NA_col_killer(.) #remove empty columns
 df_equity_clean[df_equity_clean < 0] <- NA #ignore negative values
 
-name_country <- colnames(df_equity_clean)[-1] #country names
-num_country <- ncol(df_equity_clean) - 1 #num of countries (excl Date)
-name_country_pre86 <- name_country[-c(4, 7, 10:14, 17, 19:27, 
-                                 29:30, 32:36, 39, 41:44, 
-                                 46:47)] #countries with data from before 1986
+temp_yr <- lubridate::year(df_equity_clean$Date)
+df_equity_clean <- df_equity_clean %>%
+  tibble::add_column(Year = temp_yr) %>%
+  dplyr::select(Date, Year, everything()) #adding year column
 
-year_min <- min(lubridate::year(df_equity_clean$Date)) #earliest year
-year_max <- max(lubridate::year(df_equity_clean$Date)) #latest year
+name_country <- colnames(df_equity_clean)[-1] #country names
+num_country <- ncol(df_equity_clean) - 1 #num of columns (excl Date)
+
+name_country_pre86 <- df_equity_clean %>% 
+  dplyr::filter(Year < 1986) %>%
+  func_full_NA_col_killer(.) %>%
+  dplyr::select(-c(Date, Year)) %>%
+  colnames(.)
+
+year_min <- min(df_equity_clean$Year) #earliest year
+year_max <- max(df_equity_clean$Year) #latest year
 num_years <- year_max - year_min + 1
 year_grid <- year_min:year_max
 date_grid <- df_equity$Date[-1]
@@ -52,23 +59,20 @@ date_grid <- df_equity$Date[-1]
 
 func_log_ret <- function(price_vec)
 {
-  # The following function accepts a vector of prices and returns a vector of
+  # This function accepts a vector of prices and returns a vector of
   # log returns via the formula: r_t = log(p_t) - log(p_{t-1}).
   log_pr <- log(price_vec)
   return(diff(log_pr))
 }
 
-df_log_pr <- apply(df_equity_clean[,-1], 2, func_log_ret) %>%
+df_log_pr <- apply(df_equity_clean[,-c(1,2)], 2, func_log_ret) %>%
   tibble::as_tibble(.) 
 
 # With log returns
-df_equity_clean <- cbind('Date' = date_grid, df_log_pr) %>%
+df_equity_clean <- cbind('Date' = date_grid, 
+                         'Year' = df_equity_clean$Year[-1], 
+                         df_log_pr) %>%
   tibble::as_tibble(.) 
-
-temp_yr <- lubridate::year(df_equity_clean$Date)
-df_equity_clean <- df_equity_clean %>%
-  tibble::add_column(Year = temp_yr) %>%
-  dplyr::select(Date, Year, everything())
 
 # Nesting
 
@@ -77,11 +81,44 @@ nest_df_equity <- df_equity_clean %>%
   dplyr::select(-Date) %>%
   tidyr::nest(.)
 
-nest_df_pre86 <- df_equity_clean %>% 
+# Only countries with more than 50 usable returns are 
+# allowed in the regression
+
+func_LHS_reg_country <- function(df)
+{
+  #This function accepts a dataframe, kills its
+  #full NA columns, then accepts only those 
+  #columns that have more than 50 non-missing 
+  #returns each year
+  df_1 <- func_full_NA_col_killer(df)
+  temp <- apply(df_1, 2, function(x){sum(is.na(x))})
+  temp_2 <- df_1[, temp < 315] #missing values < 50
+  
+  return(temp_2)
+}
+
+nest_df_equity_LHS <- nest_df_equity %>%
+  dplyr::mutate("LHS_country" = purrr::map(data, func_LHS_reg_country))
+
+nest_df_equity_RHS <- df_equity_clean %>% 
   dplyr::select(c(Date, Year, name_country_pre86)) %>%
   dplyr::group_by(Year) %>% 
   dplyr::select(-Date) %>%
   tidyr::nest(.)
+
+nest_df_equity_RHS <- nest_df_equity_RHS %>%
+  dplyr::mutate("RHS_data" = purrr::map(data, func_full_NA_col_killer))
+
+
+## Filling medians for missing values ##
+
+func_NA_filler <- function(vec)
+{
+  # This function fills a vector's missing values with its median 
+  vec[is.na(vec)] <- median(vec, na.rm = T)
+  return(vec)
+}
+
 
 # Changing data from wide format to long format
 # df_equity_long <- df_equity_clean %>%
